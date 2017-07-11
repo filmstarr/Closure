f2ff87381b304978ba0ad33a9d70469e002a2e0f
Ross Huelin <filmstarr@hotmail.com>
2017-07-11 23:51:05 +0100

    Add image icons. Replace favicon when setting Closure.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Closure
+Never lose an important tab again; just set a tab to require confirmation when closing or navigating away from a site. This Chrome extension will help prevent against accidental closure of tabs containing important information. Just right-click, select "Confirm Closure", and you will be alerted before important tabs are closed.
+
+## Credits
+Icon made by [Pixelbuddha](https://pixelbuddha.net/) from [www.flaticon.com](http://www.flaticon.com)
--- closure.js
+++ closure.js
@@ -1,6 +1,16 @@
 (function() {
+    //Require confirmation to close or leave the site
     window.onbeforeunload = function() {
         return "Are you sure you want to leave this site?";
     }
+
+    //Alert the user that confirmation is required to leave the site
 	alert("Confirmation now required to leave this site.");
+
+    //Replace tab favicon
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = chrome.runtime.getURL('/img/favicon.ico');
+    document.getElementsByTagName('head')[0].appendChild(link);
 })();
